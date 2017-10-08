e2d530da1c259f79f3be2116eb697043588c5551
刘16子04孺 <443540555@qq.com>
2017-10-08 22:43:52 +0800

    Create 汉诺塔递归实现.c

---
+++ 1604/401liuziru/Stack/汉诺塔递归实现.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+void move(char a,int b,char c){
+    printf("%d号盘:%c->%c\n",b,a,c);
+}
+void Hanoi(int n,char x,char y,char z){
+    if(n==1) move(x,1,z);
+    else{
+        Hanoi(n-1,x,z,y);
+        move(x,n,z);
+        Hanoi(n-1,y,x,z);
+    }
+}
+int main(){
+    Hanoi(3,'A','B','C');
+    return 0;
+}
