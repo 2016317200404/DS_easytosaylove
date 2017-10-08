252e4e5c53c6d0c4dde2f30c3a0d25130fedb9a0
yanxiao1604 <31794190+yanxiao1604@users.noreply.github.com>
2017-10-08 21:36:32 +0800

    Create 顺序表.cpp

---
+++ 1604/411yanxiao/Linklist/顺序表.cpp
@@ -0,0 +1,77 @@
+#include<iostream>
+#include<cstdio>
+
+int main()
+{
+
+    int la[50],lb[50],lc[50];
+
+    int i=0,j=0,flag=1;
+
+    int counta=0,countb=0,countc=0;
+
+
+
+/*get data*/
+
+    printf("Please enter some numbers for la[] and end by ENTER.\n");
+
+    while(getchar()!='\n')
+
+    {
+        scanf("%d",&la[i++]);
+
+    }
+
+    counta=i;
+
+    i=0;
+
+    printf("Please enter some numbers for lb[] and end by ENTER.\n");
+
+    while(getchar()!='\n')
+    {
+        scanf("%d",&lb[i++]);
+
+    }
+
+    countb=i;
+
+    i=0;
+
+/*make subtract*/
+
+    for(i=0;i<counta;i++)
+    {
+
+        for(j=0;j<countb;j++)
+        {
+
+            if(la[i]==lb[j]) flag=0;
+
+        }
+
+        if(flag)
+        {
+
+            lc[countc]=la[i];
+
+            countc++;
+
+        }
+
+        flag=1;
+
+    }
+
+/*put result out*/
+
+    printf("The result of ''la-lb'' is:\n");
+
+    for(i=0;i<countc;i++)
+    {
+        printf("%d ",lc[i]);
+    }
+    return 0;
+
+}
